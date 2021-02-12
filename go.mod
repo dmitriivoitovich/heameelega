8411543815e6b170a7b624e1b8b29b419aa1ce91
dmitrii.voitovich <dmitrii.voitovich@gmail.com>
2021-02-12 13:26:57 +0200

    add echo framework with http server

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/dmitriivoitovich/wallester-test-assignment
 
 go 1.14
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.2.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/labstack/echo/v4 v4.2.0
+	github.com/stretchr/testify v1.5.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gorm.io/gorm v1.20.12
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+const (
+	httpPort  = "80"
+	httpsPort = "443"
+
+	httpServerShutdownTimeout = time.Second * 30
+
+	httpReadTimeout  = time.Second * 10
+	httpWriteTimeout = time.Second * 10
+	httpIdleTimeout  = time.Second * 5
+	maxHeaderSize    = 1024 * 4
+)
+
+func main() {
+	// connect to DB
+	//db.Connect()
+
+	// echo instance
+	e := echo.New()
+	e.HideBanner = true
+
+	// middlewares
+	e.Use(middleware.Logger())
+
+	// routes
+	//e.GET("/", controller.ListCustomers)
+	//e.GET("/edit/:id", controller.EditCustomer)
+
+	// http server instance
+	s := &http.Server{
+		Addr:           ":" + httpPort,
+		ReadTimeout:    httpReadTimeout,
+		WriteTimeout:   httpWriteTimeout,
+		IdleTimeout:    httpIdleTimeout,
+		MaxHeaderBytes: maxHeaderSize,
+	}
+
+	go func() {
+		if err := e.StartServer(s); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), httpServerShutdownTimeout)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
